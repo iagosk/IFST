208f6e1d35c95422f52012c5253f45bc2c696488
iagosk <matheusiago083@gmail.com>
2026-09-08 10:44:27 -0300

    Instalação do React Router e setup inicial das rotas

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import { BrowserRouter, Routes, Route } from "react-router";
+import LoginView from './views/LoginView.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<App />}></Route>
+      <Route path="login" element={<LoginView />}></Route>
+    </Routes>
+  </BrowserRouter>,
 )
